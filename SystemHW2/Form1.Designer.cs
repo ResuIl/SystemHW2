8031a1a23d44c8fc4928a64571bf0dec5e3c47e1
Rəsul <71267982+ResuIl@users.noreply.github.com>
2023-02-18 02:35:37 +0400

    Add project files.

---
+++ SystemHW2/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace SystemHW2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btn_From = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.btn_Suspend = new System.Windows.Forms.Button();
+            this.btn_Resume = new System.Windows.Forms.Button();
+            this.btn_Copy = new System.Windows.Forms.Button();
+            this.btn_Abort = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "From";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(105, 23);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(411, 23);
+            this.textBox1.TabIndex = 1;
+            // 
+            // btn_From
+            // 
+            this.btn_From.Location = new System.Drawing.Point(522, 23);
+            this.btn_From.Name = "btn_From";
+            this.btn_From.Size = new System.Drawing.Size(104, 23);
+            this.btn_From.TabIndex = 2;
+            this.btn_From.Text = "File";
+            this.btn_From.UseVisualStyleBackColor = true;
+            this.btn_From.Click += new System.EventHandler(this.btn_From_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(522, 60);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(104, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(105, 60);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(411, 23);
+            this.textBox2.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(12, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 37);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "To";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 101);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(212, 23);
+            this.progressBar1.TabIndex = 6;
+            // 
+            // btn_Suspend
+            // 
+            this.btn_Suspend.Location = new System.Drawing.Point(230, 101);
+            this.btn_Suspend.Name = "btn_Suspend";
+            this.btn_Suspend.Size = new System.Drawing.Size(86, 23);
+            this.btn_Suspend.TabIndex = 7;
+            this.btn_Suspend.Text = "Suspend";
+            this.btn_Suspend.UseVisualStyleBackColor = true;
+            this.btn_Suspend.Click += new System.EventHandler(this.btn_Suspend_Click);
+            // 
+            // btn_Resume
+            // 
+            this.btn_Resume.Location = new System.Drawing.Point(322, 101);
+            this.btn_Resume.Name = "btn_Resume";
+            this.btn_Resume.Size = new System.Drawing.Size(86, 23);
+            this.btn_Resume.TabIndex = 8;
+            this.btn_Resume.Text = "Resume";
+            this.btn_Resume.UseVisualStyleBackColor = true;
+            this.btn_Resume.Click += new System.EventHandler(this.btn_Resume_Click);
+            // 
+            // btn_Copy
+            // 
+            this.btn_Copy.Location = new System.Drawing.Point(522, 101);
+            this.btn_Copy.Name = "btn_Copy";
+            this.btn_Copy.Size = new System.Drawing.Size(104, 23);
+            this.btn_Copy.TabIndex = 9;
+            this.btn_Copy.Text = "Copy";
+            this.btn_Copy.UseVisualStyleBackColor = true;
+            this.btn_Copy.Click += new System.EventHandler(this.btn_Copy_Click);
+            // 
+            // btn_Abort
+            // 
+            this.btn_Abort.Location = new System.Drawing.Point(414, 101);
+            this.btn_Abort.Name = "btn_Abort";
+            this.btn_Abort.Size = new System.Drawing.Size(102, 23);
+            this.btn_Abort.TabIndex = 10;
+            this.btn_Abort.Text = "Abort";
+            this.btn_Abort.UseVisualStyleBackColor = true;
+            this.btn_Abort.Click += new System.EventHandler(this.btn_Abort_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(639, 147);
+            this.Controls.Add(this.btn_Abort);
+            this.Controls.Add(this.btn_Copy);
+            this.Controls.Add(this.btn_Resume);
+            this.Controls.Add(this.btn_Suspend);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btn_From);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = " ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox textBox1;
+        private Button btn_From;
+        private Button button1;
+        private TextBox textBox2;
+        private Label label2;
+        private ProgressBar progressBar1;
+        private Button btn_Suspend;
+        private Button btn_Resume;
+        private Button btn_Copy;
+        private Button btn_Abort;
+        private OpenFileDialog openFileDialog1;
+        private FolderBrowserDialog folderBrowserDialog1;
+    }
+}
---
+++ SystemHW2/Form1.cs
@@ -0,0 +1,99 @@
+namespace SystemHW2
+{
+    public partial class Form1 : Form
+    {
+        bool CanAbort = false;
+        private ManualResetEvent _suspendEvent = new ManualResetEvent(true);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_From_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                textBox1.Text = openFileDialog1.FileName;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+                textBox2.Text = folderBrowserDialog1.SelectedPath;
+        }
+
+        private void btn_Copy_Click(object sender, EventArgs e)
+        {
+            Thread trd = new Thread(new ThreadStart(CopyTask));
+            trd.IsBackground = true;
+            trd.Start();
+        }
+
+        private void CopyTask()
+        {
+            CanAbort = false;
+            var file = textBox1.Text.Split("\\");
+            if (!File.Exists(textBox1.Text) && textBox2.Text.Length > 0)
+                return;
+
+            var len = 10;
+            byte[] buffer = new byte[len];
+
+            using (FileStream source = new FileStream(textBox1.Text, FileMode.Open, FileAccess.Read))
+            {
+                long fileLength = source.Length;
+                using (FileStream dest = new FileStream(textBox2.Text + "\\" + file[file.Length - 1], FileMode.CreateNew, FileAccess.Write))
+                {
+                    long totalBytes = 0;
+                    int currentBlockSize = 0;
+
+                    while ((currentBlockSize = source.Read(buffer, 0, buffer.Length)) > 0)
+                    {
+                        _suspendEvent.WaitOne();  // Rucnoy olaraq sonsuz while da yaza bilerdim bura.
+                        totalBytes += currentBlockSize;
+                        double percentage = (double)totalBytes * 100.0 / fileLength;
+                        dest.Write(buffer, 0, currentBlockSize);
+
+                        UpdateProgressBar(Convert.ToInt32(percentage));
+                        Thread.Sleep(5);
+
+                        if (CanAbort)
+                        {
+                            dest.Close();
+                            File.Delete(textBox2.Text + "\\" + file[file.Length - 1]);
+                            MessageBox.Show("Aborted.");
+                            UpdateProgressBar(0);
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        private void UpdateProgressBar(int value)
+        {
+            if (InvokeRequired)
+            {
+                Invoke(new Action<int>(UpdateProgressBar), value);
+                return;
+            }
+
+            progressBar1.Value = value;
+        }
+
+        private void btn_Abort_Click(object sender, EventArgs e)
+        {
+            CanAbort = true;
+            _suspendEvent.Set();
+        }
+
+        private void btn_Suspend_Click(object sender, EventArgs e)
+        {
+            _suspendEvent.Reset();
+        }
+
+        private void btn_Resume_Click(object sender, EventArgs e)
+        {
+            _suspendEvent.Set();
+        }
+    }
+}
